7ad739223103e426929abd469a38a17c7fa0bc16
luks19 <l.valdemoros@alumno.um.edu.ar>
2025-09-28 22:07:09 -0500

    termine el main.py

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
 from cli.cli import CLI
+
+if __name__ == "__main__":
+    # Esta es la entrada principal del programa.
+    # Crea una instancia de nuestra CLI y ejecuta el juego.
+    cli_juego = CLI()
+    cli_juego.run()
